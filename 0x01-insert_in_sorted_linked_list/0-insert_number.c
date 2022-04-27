db2f3ad8284c5cbd9f79ecb384bd661619157a85
Sarolus <cactussius.primus@gmail.com>
2022-04-27 15:44:33 +0200

    Add insert_node project

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_node - Inserts a new node in a sorted linked list.
+ * @head: beginning node of our list
+ * @number: node element
+ * Return: return the address of the new node, or NULL if failed.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new, *tmp = *head;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = number;
+
+	if (tmp->n > new->n)
+	{
+		new->next = tmp;
+		*head = new;
+		return (new);
+	}
+
+	while (tmp->next->n < new->n)
+	{
+		tmp = tmp->next;
+	}
+
+	new->next = tmp->next;
+	tmp->next = new;
+
+	return (new);
+}
