29ea8c5c960e46ce3cf0a637aed6d51379f44d8d
Sarolus <cactussius.primus@gmail.com>
2022-09-22 15:23:47 +0200

    Add interview project

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+int is_digit(char *s);
+void _puts(char *str);
+void error_message(char *s);
+char *operations(char *num1, char *num2, int len1, int len2);
+
+#endif /* HOLBERTON_H */
