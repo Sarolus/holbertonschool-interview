0e948b2c5bf16602d133915b47e3f5be749ba980
Sarolus <cactussius.primus@gmail.com>
2022-11-16 14:58:50 +0100

    Fix task 0

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""
-    Island Perimeter Calculator
-"""
+""" island module """
+
 
 def island_perimeter(grid):
     """
-        Island Perimeter Calculation Function
+        calculate the island perimeter
+        Args:
+           grid: map
+        Return:
+            perimeter
     """
 
-    perimeter = 0
-    gridHeight = len(grid) - 1
-    gridWidth = len(grid[row]) - 1
+    peri = 0
 
     for row in range(len(grid)):
         for column in range(len(grid[row])):
             if grid[row][column] == 1:
                 if row == 0 or grid[row - 1][column] == 0:
-                   perimeter +=1
+                    peri += 1
                 if column == 0 or grid[row][column - 1] == 0:
-                    perimeter +=1
-                if row == gridHeight or grid[row + 1][column] == 0:
-                    perimeter +=1
-                if column == gridWidth or grid[row][column + 1] == 0:
-                    perimeter +=1
-    
-    return perimeter
+                    peri += 1
+                if row == len(grid) - 1 or grid[row + 1][column] == 0:
+                    peri += 1
+                if column == len(grid[row]) - 1 or grid[row][column + 1] == 0:
+                    peri += 1
+    return peri
