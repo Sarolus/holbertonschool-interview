19333430bdc533c994941be10a57e32ae6a895cb
Sarolus <cactussius.primus@gmail.com>
2022-12-14 15:16:38 +0100

    Fix this shitty project

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -1,58 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "regex.h"
 
 /**
- * check_star - check if the patern is a star
+ * regex_match - checks whether a given pattern matches a given string
  *
- * @str: string to check
- * @pattern: pattern to check
+ * @str: string to scan
+ * @pattern: regular expression (RegEx)
  *
- * Return: 1 if the pattern is a star, 0 otherwise
- */
-int check_star_presence(char const *pattern) {
-	return (*(pattern + 1) == '*');
-}
-
-/**
- * check_dot - check if the next char is a dot
- *
- * @str: string to check
- * @pattern: pattern to check
- *
- * Return: 1 if the next char is a dot, 0 otherwise
- */
-int check_dot_value(char const *str, char const *pattern) {
-	if (!str || !pattern || !*str || !*pattern)
-		return (0);
-
-	return (*str == *pattern || *pattern == '.');
-}
-
-/**
- * regex_match - check if a string matches a pattern
- *
- * @str: string to check
- * @pattern: pattern to check
- *
- * Return: 1 if the string matches the pattern, 0 otherwise
+ * Return: 1 if the pattern matches the string, 0 otherwise
  */
 int regex_match(char const *str, char const *pattern)
 {
+	int dot = 0, star = 0;
+
 	if (!str || !pattern)
 		return (0);
 
-	if (!*str && !check_star_presence(pattern))
+	dot = *str && (*str == *pattern || *pattern == '.');
+	star = *(pattern + 1) == '*';
+
+	if (!*str && !star)
 		return (*pattern ? 0 : 1);
 
-	// check all characters in the string
-	else if (check_dot_value(str, pattern) && check_star_presence(pattern))
+	else if (dot && star)
 		return (regex_match(str + 1, pattern) || regex_match(str, pattern + 2));
 
-	// check only the next character in the string
-	else if (check_dot_value(str, pattern) && !check_star_presence(pattern))
+	else if (dot && !star)
 		return (regex_match(str + 1, pattern + 1));
 
-	// check if there is still a star
-	else if (check_star_presence(pattern))
+	else if (star)
 		return (regex_match(str, pattern + 2));
 
 	return (0);
--- 0x20-regex/regex.h
+++ 0x20-regex/regex.h
@@ -1,8 +1,6 @@
 #ifndef REGEX_H
 #define REGEX_H
 
-#include <stdio.h>
-
 int regex_match(char const *str, char const *pattern);
 
-#endif
+#endif /* REGEX_H */
